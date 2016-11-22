816c3cba72e6f7fd740dc20b32ce2ba138475290
Sudip kuikel <sudip.kuikel@gmail.com>
2016-11-22 10:27:33 +0545

    sum of n numbers

---
+++ menu.c
@@ -0,0 +1,21 @@
+/*
+Student Name :Sudip Kuikel
+Subject : Programming Fundamental
+Lab No. : 16
+Program : Write a Menu base program.
+Date : 2016-11-20
+*/
+#include<stdio.h>
+int main ()
+{
+	int i,n,sum=0;
+	printf("Enter numbers of terms:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		sum+=i;
+	}
+		printf("The sum of n natural numbers is %d",sum);
+		return 0;
+	}
+	
