80846f0bcb72ba4e2efa4b355844980a16dc45fa
Sudip kuikel <sudip.kuikel@gmail.com>
2017-01-17 10:48:58 +0545

    program to find cube using function

---
+++ cube.c
@@ -0,0 +1,25 @@
+/*
+  Student Name:Sudip Kuikel
+  Roll No.:23
+  Subject:Programming fundamental
+  Date:2017-01-12
+ */
+#include<stdio.h>
+#include<math.h>
+#include<stdlib.h>
+void cube();
+
+int main()
+{
+cube();
+return 0;
+}
+void cube()
+{
+int a,c;
+printf("Enter a number=");
+scanf("%d",&a);
+
+c=a*a*a;
+printf("Cube=%d",c);
+}
